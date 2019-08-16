05349d594012b96e0c325e1fc91b849b6a5c7870
aleksandr <my@gmail.com>
2019-08-16 14:44:31 +0300

    commit message

---
+++ create.php
@@ -0,0 +1,53 @@
+<?php
+//Метод Create
+
+if (isset($_POST['Description']) && isset($_POST['Name']) && isset($_POST['Created_at'])){
+    //Условие, когда форма примера сама себя
+    if ($_POST['Name'] && $_POST['Description'] && $_POST['Created_at']) {
+        //Проверены, чтобы все значения были заполнены
+
+        $Connect2DB = new PDO ("pgsql:host = localhost; dbname = postgres", "postgres", 'Khcd5028', array(\ PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+        $Create2DB = $Connect2DB -> prepare (" INSERT INTO Article (name, description, created_at) VALUES (:name, :description, :created_at )");
+        $Create2DB -> bindParam(':name', $_POST['Name']);
+        $Create2DB -> bindParam(':description', $_POST['Description']);
+        $Create2DB -> bindParam(':created_at', $_POST['Created_at']);
+        $Result = $Create2DB -> execute();
+        var_dump($_POST["Description"]);
+        var_dump($Create2DB->errorInfo());
+        exit();
+
+    }
+
+    header ("Location: index.php");
+    exit (); //Возвращаем в таблицу Index
+}
+?>
+
+<html>
+<! - форма добавления данных>
+<head>
+    <title> Create to Article </title>
+</head>
+
+<body>
+
+<form action="create.php" method="post" name="create" autocomplete="on">
+
+    <h1 style="font-size: x-large"> Введите данные </h1>
+
+    <p> <input type="text" name="Name"> Имя </p>
+    <p> <input type="text" name="Description"> Описание </p>
+    <p> <input type="date" name="Created_at" value="<?php echo date('Y-m-d H:i:s', Time());?>" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}"/> Дата создания </p>
+
+    <p>
+        <input type="submit" value="Сохранить">
+
+        <input type="reset" value="Очистить">
+
+        <button name="chancel" formaction="Index.php"> Отменить </button>
+
+    </p>
+
+</form>
+</body>
+</html>
---
+++ read.php
@@ -0,0 +1,56 @@
+<?php
+//Метод Read
+//Формируем запрос к базе данных и результат передаем в массив
+
+$Connect2DB = new PDO("pgsql:host = localhost; dbname = postgres", "postgres", 'Khcd5028', array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAME utf8'));
+$readFromDB = $Connect2DB -> prepare ("SELECT * FROM Article");
+$readFromDB -> execute();
+$Resalt = $readFromDB -> fetchAll();
+?>
+
+<!--Форма отображения данных ввиде грида-->
+<head>
+    <title> Таблица </title>
+</head>
+
+<body>
+
+
+<form action="" name="Modif" method="post">
+
+    <Table border="1" >
+
+        <!--Заголовок таблицы-->
+        <caption style=" font-size:xx-large">Article</caption>
+
+        <!--Шапка таблицы-->
+        <tr style="font-weight: bold" >
+            <td>Имя</td><td>Описание</td><td>Дата создания</td><td>Действие</td>
+
+        </tr>
+        <!--Наполнение таблицы данными-->
+        <?php
+        foreach ($Resalt as $item) { ?>
+
+        <tr>
+            <td> <?php echo $item['name']; ?> </td>
+            <td> <?php echo $item['description']; ?> </td>
+            <td> <?php echo $item['created_at']; ?> </td>
+
+            <td>
+                <a href="Update.php?id=<?php echo $item['id'];?>"> Изменить </a>
+                <a href="Delete.php?id=<?php echo $item['id'];?>"> Удалить </a>
+
+            </td>
+
+        </tr>
+
+        <?php } ?>
+    </Table>
+
+    <br>
+    <button name="create" formaction="create.php" formmethod="posr"> Добавить </button>
+
+</form>
+</body>
+</html>
